1bf7f2d758d91130849a5cb0d342888f540902b4
Yoko Harada <yokolet@gmail.com>
2019-08-29 17:26:52 -0700

    chore: simplifies expected result
    The expected output got simplified and is now just a string.

--- test/thirty_days_code/phone_number_test.clj
+++ test/thirty_days_code/phone_number_test.clj
@@ -5,9 +5,9 @@
 
 (defn phone-number-test-fixture [f]
   (core-test/wrap-test "test-data/phone-number.txt" f))
-(def expected (str (clojure.string/join "\n" ["sam=99912222" "Not found" "harry=12299933"]) "\n"))
-
 (use-fixtures :once phone-number-test-fixture)
+
+(def expected "sam=99912222\nNot found\nharry=12299933\n")
 (deftest phone-number-test
   (testing "given a name should find a phone number"
     (let [result (with-out-str (phone-number))]
